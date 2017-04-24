ade335cefa5082c135a86d2b6432c5f7dd6fdce5
Fumin Pang <fumin.pang@ninebot.com>
2017-04-24 18:26:24 +0800

    init

---
+++ cmake-build-debug/g2o/solvers/csparse/CMakeFiles/csparse_extension.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/csparse_extension.dir/csparse_helper.cpp.o"
+  "../../../../lib/libg2o_csparse_extension_d.pdb"
+  "../../../../lib/libg2o_csparse_extension_d.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/csparse_extension.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
